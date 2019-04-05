4647e32526f4ae3272e24e4a8f151ab37bd02a19
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-05 17:26:59 -0400

    .

--- Homework1/mergesort.c
+++ Homework1/mergesort.c
@@ -320,7 +320,7 @@ int main(void) {
 			int printNumber = ptr[i];
 			fprintf(f, "%d ", printNumber);
 		}
-		if (i == length) {
+		if (i == length && f != EOF) {
 			fprintf(f, "\n");
 		}
 		curFill = 0;
