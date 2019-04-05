0c74d070b1e0fd46e490e9c2233fd4620d65f5bc
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-05 17:34:02 -0400

    .

--- Homework1/mergesort.c
+++ Homework1/mergesort.c
@@ -316,10 +316,15 @@ int main(void) {
 		//printf("\nSorted array is \n");
 		//printArray(ptr, length);
 		/* print integers and floats */
-		for (i = 0; i < length; i++) {
+		for (i = 0; i < length + 1; i++) {
 			int printNumber = ptr[i];
 			if (i == length - 1) {
 				fprintf(f, "%d\n", printNumber);
+			}
+			// formats and exit condition
+			else if (i == length) {
+				fprintf(f, "%d", printNumber);
+				break;
 			}
 			else {
 				fprintf(f, "%d ", printNumber);
