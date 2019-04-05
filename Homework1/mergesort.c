438f7517c3e468fef28bcd8b756efd102c2c2216
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-05 18:00:51 -0400

    .

--- Homework1/mergesort.c
+++ Homework1/mergesort.c
@@ -322,9 +322,9 @@ int main(void) {
 			int linesPrinted = 0;
 			if (i == length - 1) {
 				fprintf(f, "%d", printNumber);
-				linesPrinted++;
-				if (linesPrinted < numOfLines) {
+				if (linesPrinted < numOfLines - 1) {
 					fprintf(f, "\n");
+					linesPrinted++;
 				}
 			}
 			//else if (numOfLines == 1 && i == length) {
