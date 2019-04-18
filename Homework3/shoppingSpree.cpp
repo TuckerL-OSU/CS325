3f1b4f61a14dfe5641864eae86d69aa4f54d5fca
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-18 16:37:08 -0400

    This is it Luigi

--- Homework3/shoppingSpree.cpp
+++ Homework3/shoppingSpree.cpp
@@ -87,7 +87,7 @@ int main() {
 		for (int j = 0; j < F; j++) {
 			sort(knapsack[j].begin(), knapsack[j].end());
 			outFile << j + 1 << ": ";
-			int size = (int)knapsack[j].size()
+			int size = (int)knapsack[j].size();
 			for (int k = 0; k < size; k++) {
 				if (k != size - 1) {
 					outFile << knapsack[j][k] << " ";
