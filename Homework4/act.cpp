05ce2ea612935c57c5f45dbe3ca091975e7176eb
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-27 09:49:39 -0400

    .

--- Homework4/act.cpp
+++ Homework4/act.cpp
@@ -330,15 +330,16 @@ struct Activity {
 
 void insertionSort(Activity arr[], int n)
 {
-	int i, key, j;
+	Activity key[n];
+	int i, j;
 	for (i = 1; i < n; i++) {
-		key = arr[i].finish;
+		key[i] = arr[i];
 		j = i - 1;
 
 		/* Move elements of arr[0..i-1], that are
 		greater than key, to one position ahead
 		of their current position */
-		while (j >= 0 && arr[j].finish > key) {
+		while (j >= 0 && arr[j].finish > key[i].finish) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
