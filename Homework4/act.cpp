eb65438dc513f79417f8608dfd1e8945f13ea945
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-27 09:47:18 -0400

    .

--- Homework4/act.cpp
+++ Homework4/act.cpp
@@ -257,76 +257,95 @@ struct Activity {
 // Merges two subarrays of arr[]. 
 // First subarray is arr[l..m] 
 // Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r)
+//void merge(int arr[], int l, int m, int r)
+//{
+//	int i, j, k;
+//	int n1 = m - l + 1;
+//	int n2 = r - m;
+//
+//	/* create temp arrays */
+//	int L[n1], R[n2];
+//
+//	/* Copy data to temp arrays L[] and R[] */
+//	for (i = 0; i < n1; i++)
+//		L[i] = arr[l + i];
+//	for (j = 0; j < n2; j++)
+//		R[j] = arr[m + 1 + j];
+//
+//	/* Merge the temp arrays back into arr[l..r]*/
+//	i = 0; // Initial index of first subarray 
+//	j = 0; // Initial index of second subarray 
+//	k = l; // Initial index of merged subarray 
+//	while (i < n1 && j < n2)
+//	{
+//		if (L[i] <= R[j])
+//		{
+//			arr[k] = L[i];
+//			i++;
+//		}
+//		else
+//		{
+//			arr[k] = R[j];
+//			j++;
+//		}
+//		k++;
+//	}
+//
+//	/* Copy the remaining elements of L[], if there
+//	are any */
+//	while (i < n1)
+//	{
+//		arr[k] = L[i];
+//		i++;
+//		k++;
+//	}
+//
+//	/* Copy the remaining elements of R[], if there
+//	are any */
+//	while (j < n2)
+//	{
+//		arr[k] = R[j];
+//		j++;
+//		k++;
+//	}
+//}
+//
+///* l is for left index and r is right index of the
+//sub-array of arr to be sorted */
+//void mergeSort(int arr[], int l, int r)
+//{
+//	if (l < r)
+//	{
+//		// Same as (l+r)/2, but avoids overflow for 
+//		// large l and h 
+//		int m = l + (r - l) / 2;
+//
+//		// Sort first and second halves 
+//		mergeSort(arr, l, m);
+//		mergeSort(arr, m + 1, r);
+//
+//		merge(arr, l, m, r);
+//	}
+//}
+
+void insertionSort(Activity arr[], int n)
 {
-	int i, j, k;
-	int n1 = m - l + 1;
-	int n2 = r - m;
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+		key = arr[i].finish;
+		j = i - 1;
 
-	/* create temp arrays */
-	int L[n1], R[n2];
-
-	/* Copy data to temp arrays L[] and R[] */
-	for (i = 0; i < n1; i++)
-		L[i] = arr[l + i];
-	for (j = 0; j < n2; j++)
-		R[j] = arr[m + 1 + j];
-
-	/* Merge the temp arrays back into arr[l..r]*/
-	i = 0; // Initial index of first subarray 
-	j = 0; // Initial index of second subarray 
-	k = l; // Initial index of merged subarray 
-	while (i < n1 && j < n2)
-	{
-		if (L[i] <= R[j])
-		{
-			arr[k] = L[i];
-			i++;
+		/* Move elements of arr[0..i-1], that are
+		greater than key, to one position ahead
+		of their current position */
+		while (j >= 0 && arr[j].finish > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
 		}
-		else
-		{
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-
-	/* Copy the remaining elements of L[], if there
-	are any */
-	while (i < n1)
-	{
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-
-	/* Copy the remaining elements of R[], if there
-	are any */
-	while (j < n2)
-	{
-		arr[k] = R[j];
-		j++;
-		k++;
+		arr[j + 1] = key;
 	}
 }
 
-/* l is for left index and r is right index of the
-sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
-{
-	if (l < r)
-	{
-		// Same as (l+r)/2, but avoids overflow for 
-		// large l and h 
-		int m = l + (r - l) / 2;
-
-		// Sort first and second halves 
-		mergeSort(arr, l, m);
-		mergeSort(arr, m + 1, r);
-
-		merge(arr, l, m, r);
-	}
-}
 
 //comparator helper function for sort
 //int compare(Activity x, Activity y) {
@@ -377,7 +396,8 @@ void mergeSort(int arr[], int l, int r)
 
 void lastToStart(Activity activities[], int numActivities) {
 	/*sort(vec.begin(), vec.end(), compare);*/
-	mergeSort(activities[]->finish, 0, numActivities - 1);
+	//mergeSort(activities[]->finish, 0, numActivities - 1);
+	insertionSort(activities, numActivities);
 	/*for (int i = 0; i < vec.size(); i++)
 	cout << vec[i].number << " " << vec[i].start << " " << vec[i].finish << endl;
 	verify sort (debug)*/
@@ -395,7 +415,7 @@ void lastToStart(Activity activities[], int numActivities) {
 
 	//continue to add the activity with the next closest start time that does not conflict
 	//with activities already added
-	for (int i = 1; i < activities.sizeof(); i++) {
+	for (int i = 1; i < numActivities; i++) {
 		if (activities[i].start != activities[i - 1].start && activities[i].finish <= subset.back().start)
 		{
 			subset[i] = activities[i];
@@ -406,7 +426,7 @@ void lastToStart(Activity activities[], int numActivities) {
 	cout << "Number of activities selected = " << actCounter << endl;
 
 	//print out activity numbers selected
-	for (int j = subset.sizeof() - 1; j >= 0; j--)
+	for (int j = actCounter - 1; j >= 0; j--)
 		cout << subset[j].actNum << " ";
 	cout << endl << endl;
 }
