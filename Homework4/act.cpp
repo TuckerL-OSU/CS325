8df35bad90121d7dc2519b24e8faccb664a9cfef
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-28 14:04:31 -0400

    .

--- Homework4/act.cpp
+++ Homework4/act.cpp
@@ -114,34 +114,35 @@ void lastToStart(Activity activities[], int numActivities) {
 
 	//continue to add the activity with the next closest start time that does not conflict
 	//with activities already added
-	//for (int i = 1; i < numActivities; i++) {
-	//	if (activities[i].start != activities[i - 1].start) {
-	//		if (activities[i].start >= subset[actCounter - 1].finish) {
-	//			subset[actCounter].actNum = activities[i].actNum;
-	//			subset[actCounter].start = activities[i].start;
-	//			subset[actCounter].finish = activities[i].finish;
+	// 2 8 11
+	for (int i = 1; i < numActivities; i++) {
+		if (activities[i].start != activities[i - 1].start) {
+			if (activities[i].start >= subset[actCounter - 1].finish) {
+				subset[actCounter].actNum = activities[i].actNum;
+				subset[actCounter].start = activities[i].start;
+				subset[actCounter].finish = activities[i].finish;
 
-	//			actCounter++;
-	//			//continue;
-	//		}
-	//		//else if (activities[i].start >= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].start) {
-	//		//	subset[actCounter - 1].actNum = activities[i].actNum;
-	//		//	subset[actCounter - 1].start = activities[i].start;
-	//		//	subset[actCounter - 1].finish = activities[i].finish;
-	//		//}
-	//		else if (activities[i].start >= subset[actCounter - 1].start && activities[i].start <= subset[actCounter - 1].finish) {
-	//			subset[actCounter - 1].actNum = activities[i].actNum;
-	//			subset[actCounter - 1].start = activities[i].start;
-	//			subset[actCounter - 1].finish = activities[i].finish;
-	//		}
-	//		//else if (activities[i].finish >= subset[actCounter - 1].finish) {
-	//		//	subset[actCounter - 1].actNum = activities[i].actNum;
-	//		//	subset[actCounter - 1].start = activities[i].start;
-	//		//	subset[actCounter - 1].finish = activities[i].finish;
-	//		//}
+				actCounter++;
+				//continue;
+			}
+			//else if (activities[i].start >= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].start) {
+			//	subset[actCounter - 1].actNum = activities[i].actNum;
+			//	subset[actCounter - 1].start = activities[i].start;
+			//	subset[actCounter - 1].finish = activities[i].finish;
+			//}
+			else if (activities[i].start >= subset[actCounter - 1].start && activities[i].start >= subset[actCounter - 1].finish) {
+				subset[actCounter - 1].actNum = activities[i].actNum;
+				subset[actCounter - 1].start = activities[i].start;
+				subset[actCounter - 1].finish = activities[i].finish;
+			}
+			//else if (activities[i].finish >= subset[actCounter - 1].finish) {
+			//	subset[actCounter - 1].actNum = activities[i].actNum;
+			//	subset[actCounter - 1].start = activities[i].start;
+			//	subset[actCounter - 1].finish = activities[i].finish;
+			//}
 
-	//	}
-	//}
+		}
+	}
 
 	cout << "Number of activities selected = " << actCounter << endl;
 
