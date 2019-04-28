87e793da561ed80befcff605823c7935987f8829
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-28 14:44:06 -0400

    .

--- Homework4/act.cpp
+++ Homework4/act.cpp
@@ -128,11 +128,13 @@ void lastToStart(Activity activities[], int numActivities) {
 			}
 
 			// 11
-			else if (activities[i].start >= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].start) {
-				subset[actCounter - 1].actNum = activities[i].actNum;
-				subset[actCounter - 1].start = activities[i].start;
-				subset[actCounter - 1].finish = activities[i].finish;
-			}
+			//else if (activities[i].start >= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].start) {
+			//	subset[actCounter - 1].actNum = activities[i].actNum;
+			//	subset[actCounter - 1].start = activities[i].start;
+			//	subset[actCounter - 1].finish = activities[i].finish;
+			//}
+
+
 
 			//if (/*activities[i].start >= subset[actCounter - 1].start &&*/ activities[i].finish <= subset[actCounter - 1].finish) {
 			//	subset[actCounter - 1].actNum = activities[i].actNum;
@@ -149,6 +151,11 @@ void lastToStart(Activity activities[], int numActivities) {
 			//	subset[actCounter - 1].start = activities[i].start;
 			//	subset[actCounter - 1].finish = activities[i].finish;
 			//}
+		}
+		else if (activities[i].start >= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].start) {
+			subset[actCounter - 1].actNum = activities[i].actNum;
+			subset[actCounter - 1].start = activities[i].start;
+			subset[actCounter - 1].finish = activities[i].finish;
 		}
 	}
 
