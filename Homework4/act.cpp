9a580e21d7e1380d0ff6276b70d9446fb3a2f4e0
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-04-28 14:51:40 -0400

    .

--- Homework4/act.cpp
+++ Homework4/act.cpp
@@ -141,13 +141,13 @@ void lastToStart(Activity activities[], int numActivities) {
 			//	subset[actCounter - 1].start = activities[i].start;
 			//	subset[actCounter - 1].finish = activities[i].finish;
 			//}
-			else if (activities[i].finish >= subset[actCounter - 1].finish && activities[i].start <= subset[actCounter - 1].start) {
-				subset[actCounter].actNum = activities[i].actNum;
-				subset[actCounter].start = activities[i].start;
-				subset[actCounter].finish = activities[i].finish;
+			//else if (activities[i].finish >= subset[actCounter - 1].finish && activities[i].start <= subset[actCounter - 1].start) {
+			//	subset[actCounter].actNum = activities[i].actNum;
+			//	subset[actCounter].start = activities[i].start;
+			//	subset[actCounter].finish = activities[i].finish;
 
-				actCounter++;
-			}
+			//	actCounter++;
+			//}
 			//else if (activities[i].start <= subset[actCounter - 1].start && activities[i].finish >= subset[actCounter - 1].finish) {
 			//	subset[actCounter - 1].actNum = activities[i].actNum;
 			//	subset[actCounter - 1].start = activities[i].start;
