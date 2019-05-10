75087bdd865e9e0c8b4893ae365ffabea0d6d882
TuckerL-OSU <38082010+TuckerL-OSU@users.noreply.github.com>
2019-05-10 13:15:23 -0400

    .

--- Homework5/wrestler.cpp
+++ Homework5/wrestler.cpp
@@ -52,12 +52,12 @@ int getIndex(vector<string> values, string source) {
 
 // make connections within the graph
 void addEdge(vector < vector <wrestler*> >& graph, string A, string B, vector <string> reference) {
-	int source = getIndex(reference, A);
-	int partner = getIndex(reference, B);
+	int wrestler = getIndex(reference, A);
+	int rival = getIndex(reference, B);
 
-	// creating the pairs 
-	graph[source].push_back(graph[partner][0]);
-	graph[partner].push_back(graph[source][0]);
+	// creating the edges 
+	graph[wrestler].push_back(graph[rival][0]);
+	graph[rival].push_back(graph[wrestler][0]);
 }
 
 // check if our graph of wrestlers can provide non conflicting rivalries
@@ -90,26 +90,22 @@ void bfs(vector < vector <wrestler*> >& graph, vector <string> reference) {
 				for (int x = 1; x < (int)currentNode.size(); x++) {
 					// condition to check if the neighbor has been previously discovered
 					if (currentNode[x]->visited == false) {
-						// here is how we assign the teams
-						/*
-						basically if the current wrestler is a babyface than its neighbors must be heels
-						we first check what team the neighbors have
-						if they are unknown, than set it to the opposite team from the current vertex
-						if they have the same team as the predecessor than we end the traversal because this signals a conflict
-						*/
+						// assign the teams
+						// set the neighbor to heel if the predecessor is a babyface
 						if (currentNode[x]->team == NONE && currentNode[0]->team == BABY) {
-							currentNode[x]->team = HEEL; // set the neighbor to heel if the predecessor is a babyface
+							currentNode[x]->team = HEEL;
 
-							heels.push_back(currentNode[x]->name); // updating our list of heels
+							heels.push_back(currentNode[x]->name);
 						}
+						// set the neighbor to babyface if the predecessor is a heel
 						else if (currentNode[x]->team == NONE && currentNode[0]->team == HEEL) {
-							currentNode[x]->team = BABY; // set the neighbor to babyface if the predecessor is a heel
+							currentNode[x]->team = BABY; 
 
-							babyFaces.push_back(currentNode[x]->name);  // updating our list of babyfaces
+							babyFaces.push_back(currentNode[x]->name);
 						}
+						// conflict found
 						else if (currentNode[x]->team == currentNode[0]->team) {
-							// conflict found
-							cout << "Impossible" << endl;
+							cout << "No" << endl;
 							return;
 						}
 
@@ -122,7 +118,7 @@ void bfs(vector < vector <wrestler*> >& graph, vector <string> reference) {
 	}
 
 	// showcasing the result
-	cout << "Yes possible" << endl;
+	cout << "Yes" << endl;
 	cout << "Babyfaces: ";
 	for (int x = 0; x < (int)babyFaces.size(); x++) {
 		cout << babyFaces[x] << " ";
